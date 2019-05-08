1fcf13804b98ea24b8532bbb16a38c6ac3c56f47
luyixin <410780496@qq.com>
2019-05-08 23:13:09 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -6,43 +6,46 @@
   </div>
 </template>
 <script>
-import { Base64 } from 'js-base64'
+// import { Base64 } from 'js-base64'
 import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
-import { routes } from '@/config/router'
+// import { routes } from '@/config/router'
 
 export default {
   data () {
     return {
-      locale: zhCN,
-      userInfo: window.localStorage.getItem('userInfo'),
-      menusId: []
-    }
-  },
-  watch: {
-    '$route' (v) {
-      if (!this.userInfo || v.fullPath === '/login' || v.fullPath === '/recover') return
-      !this.menusId.includes(v.meta.id) && this.redirectUrl(routes)
-    }
-  },
-  mounted () {
-    if (!this.userInfo) return this.$router.replace('/login')
-    try {
-      this.menusId = JSON.parse(Base64.decode(Base64.decode(this.userInfo))).menus.map(a => +a)
-      !this.menusId.includes(this.$route.meta.id) && this.redirectUrl(routes)
-    } catch (e) {
-      this.$router.replace('/login')
-    }
-  },
-  methods: {
-    redirectUrl (routes) {
-      for (let i = 0; i < routes.length; i++) {
-        if (routes[i].meta && routes[i].meta.id && this.menusId.includes(routes[i].meta.id)) {
-          return this.$router.replace(routes[i].meta.fullPath)
-        }
-        routes[i].children && this.redirectUrl(routes[i].children)
-      }
+      locale: zhCN
+      // userInfo: window.localStorage.getItem('userInfo'),
+      // menusId: []
     }
   }
+  // watch: {
+  //   '$route' (v) {
+  //     if (!this.userInfo || v.fullPath === '/login' || v.fullPath === '/recover') return
+  //     this.$nextTick(() => {
+  //       console.log(v.meta)
+  //       !this.menusId.includes(v.meta.id) && this.redirectUrl(routes)
+  //     })
+  //   }
+  // },
+  // mounted () {
+  //   if (!this.userInfo) return this.$router.replace('/login')
+  //   try {
+  //     this.menusId = JSON.parse(Base64.decode(Base64.decode(this.userInfo))).menus.map(a => +a)
+  //     !this.menusId.includes(this.$route.meta.id) && this.redirectUrl(routes)
+  //   } catch (e) {
+  //     this.$router.replace('/login')
+  //   }
+  // }
+  // methods: {
+  //   redirectUrl (routes) {
+  //     for (let i = 0; i < routes.length; i++) {
+  //       if (routes[i].meta && routes[i].meta.id && this.menusId.includes(routes[i].meta.id)) {
+  //         return this.$router.replace(routes[i].meta.fullPath)
+  //       }
+  //       routes[i].children && this.redirectUrl(routes[i].children)
+  //     }
+  //   }
+  // }
 }
 </script>
 <style lang="less">
